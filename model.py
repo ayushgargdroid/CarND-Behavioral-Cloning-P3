6abca65f78c208957eb885c3da634eb76083d91c
ayushgargdroid <ayushgargdroid@gmail.com>
2020-01-25 21:12:42 +0000

    Initial training working

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ from matplotlib import image as mpimg
 import cv2
 import math
 import numpy as np
+from sklearn.model_selection import train_test_split
 import csv
 import os
 import tensorflow as tf
@@ -15,7 +16,7 @@ from keras.layers.pooling import MaxPooling2D
 from keras.optimizers import Adam
 
 learning_rate = 0.001
-batch_size = 128
+batch_size = 8
 epochs = 5
 
 center_imgs_name = []
@@ -32,6 +33,11 @@ with open('./data/driving_log.csv', mode='r') as infile:
         steering.append(float(rows[3]))
 
 steering = np.array(steering)
+center_imgs_name = np.array(center_imgs_name)
+print(steering.shape)
+print(center_imgs_name.shape)
+x_train, x_valid, y_train, y_valid = train_test_split(center_imgs_name, steering,test_size = 0.4)
+x_valid, x_test, y_valid, y_test = train_test_split(x_valid, y_valid, test_size = 0.5)
 
 def batch_generator(center_name,left_name,right_name,steering_list,operation):
     test_img = cv2.imread('./data/'+center_name[0])
@@ -54,17 +60,22 @@ def batch_generator(center_name,left_name,right_name,steering_list,operation):
     index = 0
     for i in np.random.permutation(range(start,start+size)):
         image = cv2.imread('./data/'+center_name[i])[60:-25,:,:]
+        if(not np.any(image)):
+            print('iter: '+str(i)+' zeros: '+str(not np.any(image)))
         image = cv2.cvtColor(image,cv2.COLOR_BGR2YUV)
         images[index] = image
         steering[index] = steering_list[i]
-    yield images,steering
+        index += 1
+    return images,steering
 
-tt = batch_generator(center_imgs_name,None,None,steering,'viz')
-sample_img, _ = next(tt)
+sample_img,_ = batch_generator(x_train[:1],None,None,y_train[:1],'viz')
 sample_img = np.uint8(sample_img[0])
 
+x_train, y_train = batch_generator(x_train,None,None,y_train,'train')
+x_valid, y_valid = batch_generator(x_valid,None,None,y_valid,'valid')
+
 model = Sequential()
-model.add(Lambda(lambda x: (x-127.5)/127.5,input_shape=sample_img.shape))
+model.add(Lambda(lambda x: (x-127.5)/127.5,input_shape=(75,320,3)))
 model.add(Conv2D(24, 5, 5, activation='elu', subsample=(2, 2)))
 model.add(Conv2D(36, 5, 5, activation='elu', subsample=(2, 2)))
 model.add(Conv2D(48, 5, 5, activation='elu', subsample=(2, 2)))
@@ -79,4 +90,6 @@ model.add(Dense(1))
 model.summary()
 
 model.compile(loss='mean_squared_error', optimizer=Adam(lr=learning_rate))
-model.fit_generator(batch_generator(center_imgs_name,None,None,steering,'train'),batch_size,epochs,validation_data=batch_generator(center_imgs_name,None,None,steering,'valid'),nb_val_samples=0.2*len(center_imgs_name),verbose=1)
+# model.fit_generator(batch_generator(x_train,None,None,y_train,x_train.shape[0]),batch_size,epochs,validation_data=batch_generator(x_valid,None,None,y_valid,x_valid.shape[0]),nb_val_samples=x_valid.shape[0],verbose=1)
+
+model.fit(x_train, y_train, batch_size, epochs, validation_data = (x_valid, y_valid))
